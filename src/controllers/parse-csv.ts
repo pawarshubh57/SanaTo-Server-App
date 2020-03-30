ef31b0a63d8bd8a22e456125ce0a597c346500d7
sonawane-yogesh <sonawane-yogesh@users.noreply.github.com>
2020-03-30 10:10:59 +0530

    last change

--- src/controllers/parse-csv.ts
+++ src/controllers/parse-csv.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { csvFileParsing } from '../helpers';
 
 const parseCsv = function(request: Request, response: Response) {
-  const isParsed: boolean = csvFileParsing
-  .parseCsv('D:\\Flo-Kapture-Mean-Stack-Projects\\Flo-Kapture-NodeJs-TypeScript-App\\dist\\ExtractedProjects\\CCSES-20180829\\I-Descriptors\\AC.FLE.csv', ',');
-  
-  response.status(200).send(isParsed).end();
+  let filePath: string =
+    'D:\\Flo-Kapture-Mean-Stack-Projects\\Flo-Kapture-NodeJs-TypeScript-App\\dist\\ExtractedProjects\\CCSES-20180829\\I-Descriptors\\AC.FLE.csv';
+  const isParsed: boolean = csvFileParsing.parseCsv(filePath, ',');
+
+  response
+    .status(200)
+    .send(isParsed)
+    .end();
 };
 
 export { parseCsv };
