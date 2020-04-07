55a3e8e621cee359b85ff605098f45a471fb94c2
sonawane-yogesh <sonawane-yogesh@users.github.com>
2020-04-07 16:35:22 +0530

    error-obj-correction

--- src/helpers/static-helper/model-validation.ts
+++ src/helpers/static-helper/model-validation.ts
@@ -5,8 +5,8 @@ import isEmail from 'validator';
 import validator from 'validator';
 
 class ModelValidation {
-  public validateObj = async function(body: any): Promise<Array<ErrorField>> {
-    const errors: Array<ErrorField> = [];
+  public validateObj = async function (body: any): Promise<Array<ErrorField>> {
+    let errors: Array<ErrorField> = [];
     const primaryKeyFields: Array<string> = body.PrimaryKeyFields;
     const data: any = body.Data as any;
     const keys = Object.keys(data);
@@ -15,7 +15,7 @@ class ModelValidation {
       $and: [{ Columns: { $all: keys } }, { Columns: { $size: keys.length } }],
     };
     const model = await sanaToService.StaticModel.getItem(filter);
-    console.log(model);
+    if (model === null) return [];
     const dates: Array<DateField> = model.DateField;
     const others: Array<OtherFields> = model.OtherFields;
 
@@ -23,20 +23,12 @@ class ModelValidation {
       for (let d in data) {
         dates.find((v: DateField) => {
           if (v.dateColumn === d) {
-            console.log('Date Field', v);
             var date: Date = data[d];
-            // var test = isDate(data[d]);
-            // check format of Date
-            // changes done...
-            // var isValidDate = moment('02/12/2020', v.format, true).isValid();
-            console.log('isValidDate and format ', isValidDate);
-            // check format of Date
-            var testDate = moment(date);
-            // var testFormat = moment.utc().format(v.format);
-            var mDate = moment(date, v.format, true);
-            var rtt = mDate.isValid();
-            console.log(rtt);
-            var isValidDate = moment.parseZone(testDate, v.format, true).isValid();
+            // var testDate = moment(date);
+            // var mDate = moment(date, v.format, true);
+            // var rtt = mDate.isValid();
+            // console.log(rtt);
+            var isValidDate = moment.parseZone(date, v.format, true).isValid();
 
             if (!isValidDate) {
               errors.push({
@@ -52,19 +44,19 @@ class ModelValidation {
             if (o.type === 'string') {
               var value: string = data[d];
               var type = 'string';
-              this.errors = this.checkNull(value, o, errors);
-              this.errors = this.checkType(value, o, errors, type);
-              this.errors = this.checkLength(value, o, errors);
-              this.errors = o.isEmail ? this.checkEmail(value, o, errors) : [];
+              this.checkNull(value, o, errors);
+              this.checkType(value, o, type, errors);
+              this.checkLength(value, o, errors);
+              o.isEmail ? this.checkEmail(value, o, errors) : [];
             } else if (o.type === 'number') {
               var val1 = data[d];
               var val = Number.parseInt(data[d]);
               if (val1 === null) {
-                this.errors = this.checkNull(val1, o, errors);
+                this.checkNull(val1, o, errors);
               } else if (isNaN(val)) {
-                this.errors = this.checkNanValue(val, o, errors);
+                this.checkNanValue(val, o, errors);
               } else {
-                this.errors = this.checkMinMaxValue(val, o, errors);
+                this.checkMinMaxValue(val, o, errors);
               }
             }
           }
@@ -76,82 +68,65 @@ class ModelValidation {
       return errors;
     }
   };
-  public checkEmail = function(
-    value: string,
-    o: OtherFields,
-    errors: Array<ErrorField>
-  ): Array<ErrorField> {
-    var test = validator.isEmail(value);
+  public checkEmail = function (value: string, o: OtherFields, errors: Array<ErrorField>){
+    // var test = validator.isEmail(value);
     var testRegex = new RegExp(o.regex);
     var isEmailCheck = testRegex.test(value);
     if (isEmailCheck) return errors;
-    return this.errors.push({
+   return errors.push({
       column: o.fieldName,
       value: value,
       msg: `${o.fieldName} is not valid`,
+      error: null
     });
   };
-  public checkLength = function(value: string, o: OtherFields, errors: Array<ErrorField>) {
+  public checkLength = function (value: string, o: OtherFields, errors: Array<ErrorField>) {
     var lengthCheck =
       (o.maxLength === 0 && o.minLength === 0) ||
-      (value.length <= o.maxLength && value.length >= o.minLength)
+        (value.length <= o.maxLength && value.length >= o.minLength)
         ? true
         : false;
     if (lengthCheck) return errors;
-    return this.errors.push({
+    return errors.push({
       column: o.fieldName,
       value: value,
       msg: `Length of ${o.fieldName} must be between ${o.maxLength} and ${o.minLength}`,
     });
   };
-  public checkNull = function(
-    value: string | number,
-    o: OtherFields,
-    errors: Array<ErrorField>
-  ): Array<ErrorField> {
+  public checkNull = function (value: string | number, o: OtherFields, errors: Array<ErrorField>) {
     var nullCheck = (value === null) === o.canBeNull ? true : false;
     if (nullCheck) return errors;
-    return this.errors.push({
+    return errors.push({
       column: o.fieldName,
       value: value,
       msg: `${o.fieldName} can not null`,
     });
   };
-  public checkType = function(
-    value: string,
-    o: OtherFields,
-    errors: Array<ErrorField>,
-    type: any
-  ): Array<ErrorField> {
+  public checkType = function (value: string, o: OtherFields, type: any, errors: Array<ErrorField>) {
     var typeCheck = typeof value === type ? true : false;
     if (typeCheck) return errors;
-    return this.errors.push({
+    return errors.push({
       column: o.fieldName,
       value: value,
       msg: `${o.fieldName} is not of type ${o.type}`,
     });
   };
-  public checkNanValue = function(
-    value: any,
-    o: OtherFields,
-    errors: Array<ErrorField>
-  ): Array<ErrorField> {
+  public checkNanValue = function (value: any, o: OtherFields, errors: Array<ErrorField>) {
     if (isNaN(value)) {
-      return this.errors.push({
+      return errors.push({
         column: o.fieldName,
         value: value,
         msg: `${o.fieldName} must be Number`,
       });
     }
-    return errors;
   };
-  public checkMinMaxValue = function(value: number, o: OtherFields, errors: Array<ErrorField>) {
+  public checkMinMaxValue = function (value: number, o: OtherFields, errors: Array<ErrorField>) {
     var valueCheck =
       (o.minValue === 0 && o.maxValue === 0) || (value <= o.maxValue && value >= o.minValue)
         ? true
         : false;
     if (valueCheck) return errors;
-    return this.errors.push({
+    return errors.push({
       column: o.fieldName,
       value: value,
       msg: `Value must be between ${o.minValue} and ${o.maxValue}`,
